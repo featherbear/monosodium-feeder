55598dc3543aae9d42cea57f37805dac08c7f98e
Andrew Wong <andrew.j.wong@outlook.com>
2019-08-14 17:07:40 +1000

    Update Client.js to extend EventEmitter

--- Client.js
+++ Client.js
@@ -1,47 +1,53 @@
-'use strict';
+'use strict'
 
 let login = require('facebook-chat-api')
+let EventEmitter = require('events')
 
+async function spawnClient (creds) {
+  let client = new class extends EventEmitter {
+    constructor () {
+      super()
+      this.api = null
+    }
 
-async function spawnClient(creds) {
-  let client = new class {    
-    constructor() {
-      this.api = null;
+    get id () {
+      if (this.api === null) throw Error('Bot not logged in')
+      return Number(this.api.getCurrentUserID())
     }
-    
-    get id() {
-      if (this.api === null) throw "Bot not logged in"
-      return this.api.getCurrentUserID();
-    }
-    
-    async login(credentials) {
+
+    async login (credentials) {
       return new Promise((resolve, reject) => {
         login(credentials, (err, api) => {
-          if (err) reject(err);
-          this.api = api;        
-          resolve(this);
-        })  
-      });
+          if (err) reject(err)
+          this.api = api
+          resolve(this)
+        })
+      })
     }
-  }();
+  }()
 
-  await client.login(creds);
-  
+  await client.login(creds)
+
   client.api.setOptions({
-    logLevel: "warn"
-    ,selfListen: true
-    ,listenEvents: true
-    // ,forceLogin: true
+    logLevel: 'warn',
+    selfListen: true,
+    listenEvents: true,
+    forceLogin: true
   })
-  
+
   client.api.listen((err, message) => {
-    if (err) return console.error(err);
-    
-    console.log(message);
+    if (err) return console.error(err)
+    console.log(message)
+    client.emit('event', {
+      body: message.body,
+      sender: Number(message.senderID),
+      thread: Number(message.threadID),
+      timestamp: Number(message.timestamp),
+      account: client.id
+    })
   })
-  
-  return client;
+
+  return client
 }
 
- 
 module.exports = spawnClient
